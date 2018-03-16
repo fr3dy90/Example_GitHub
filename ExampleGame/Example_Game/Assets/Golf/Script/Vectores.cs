610dda1cb2f087be3279d81989c66263d33fefea
fr3dy <jfr3dy90@gmail.com>
2018-03-15 21:04:56 -0500

    Pruebas

---
+++ ExampleGame/Example_Game/Assets/Golf/Script/Vectores.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vectores : MonoBehaviour {
+
+    public bool simulate;
+    public GameObject proyectile;
+    public Vector2 proyectilePos;
+    public Vector2 velocity;
+    public Vector2 gravity = new Vector2(0, -9.8f);
+
+    public bool hit;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        proyectile.transform.position = proyectilePos;
+        rb = proyectile.GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (simulate)
+        {
+            velocity += gravity*Time.deltaTime;
+            proyectilePos += velocity*Time.deltaTime;
+            proyectile.transform.position = proyectilePos;
+        }
+        if (hit)
+        {
+            hit = false;
+            rb.useGravity = true;
+            rb.AddForce(velocity, ForceMode.Impulse);
+        }
+    }
+}
