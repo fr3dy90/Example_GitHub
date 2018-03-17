1165d639410a9dea14e963a14ebedc89d4b68469
fr3dy <jfr3dy90@gmail.com>
2018-03-16 20:54:20 -0500

    Campo

--- ExampleGame/Example_Game/Assets/Golf/Script/Vectores.cs
+++ ExampleGame/Example_Game/Assets/Golf/Script/Vectores.cs
@@ -6,23 +6,28 @@ public class Vectores : MonoBehaviour {
 
     public bool simulate;
     public GameObject proyectile;
-    public Vector2 proyectilePos;
-    public Vector2 velocity;
-    public Vector2 gravity = new Vector2(0, -9.8f);
+    Vector3 initPos;
+    public Vector3 proyectilePos;
+    public Vector3 velocity;
+    public Vector3 gravity = new Vector3(0, -9.8f, 0);
 
     public bool hit;
     Rigidbody rb;
 
     public int res;
-    public Vector2[] points;
+    public Vector3[] points;
     public float timeSimulate;
+
+    LineRenderer lr;
 
 	// Use this for initialization
 	void Start ()
     {
+        lr = GetComponent<LineRenderer>();
+        initPos = proyectile.transform.position;
         proyectile.transform.position = proyectilePos;
         rb = proyectile.GetComponent<Rigidbody>();
-        points = new Vector2[res];
+        points = new Vector3[res];
     }
 	
 	// Update is called once per frame
@@ -49,18 +54,21 @@ public class Vectores : MonoBehaviour {
 
     void Calculate()
     {
-        Vector2 refVel = velocity;
-        Vector2 refG = gravity;
+        Vector3 refVel = velocity;
+        Vector3 refG = gravity;
+        
         for (int i = 0; i < res; i++)
         {
             float t = i / (float)res*timeSimulate;
-            Vector2 desp = refVel * t + refG * t * t / 2f;
+            Vector3 desp = refVel * t + refG * t * t / 2f;
+            desp += initPos;
             points[i] = desp;
             if (i > 0)
             {
 
                 Debug.DrawLine(points[i], points[i-1], Color.green);
             }
+            lr.SetPositions(points);
         }
     }
 }
