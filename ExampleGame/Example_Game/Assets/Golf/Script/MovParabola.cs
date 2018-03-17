6781a7a4d7b1d73b25a449f48f003514a41d84ab
fr3dy <jfr3dy90@gmail.com>
2018-03-17 13:02:07 -0500

    Campo

---
+++ ExampleGame/Example_Game/Assets/Golf/Script/MovParabola.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovParabola : MonoBehaviour {
+
+    public int angle;
+    public float res;
+    public float disx;
+    public float t;
+
+	// Use this for initialization
+	void Start () {
+        res = Mathf.Cos(angle);
+        t = disx * res;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
